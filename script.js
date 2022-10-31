5092d2ade074548531685aba0d76c95c97a10a80
Naomi Maindret <naomi.maindret@hotmail.fr>
2022-10-31 16:08:18 +0100

    change the number of color

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let containerEl = document.querySelector(".container");
 
-for (let index = 0; index < 6; index++) {
+for (let index = 0; index < 25; index++) {
     const colorContainerEl = document.createElement("div");
     colorContainerEl.classList.add("color-container");
     containerEl.appendChild(colorContainerEl);
