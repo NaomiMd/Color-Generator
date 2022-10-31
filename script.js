24db0a4edfd9b414d80dd97542bf707d930f4abd
Naomi Maindret <naomi.maindret@hotmail.fr>
2022-10-31 15:48:09 +0100

    add function generate color and random color

--- script.js
+++ script.js
@@ -4,5 +4,28 @@ for (let index = 0; index < 6; index++) {
     const colorContainerEl = document.createElement("div");
     colorContainerEl.classList.add("color-container");
     containerEl.appendChild(colorContainerEl);
-    
 }
+
+let colorContainerEls = document.querySelectorAll(".color-container");
+
+generateColors();
+
+function generateColors(){
+    colorContainerEls.forEach((colorContainerEl)=>{
+        const newColorCode = randomColor();
+        colorContainerEl.style.backgroundColor = "#" + newColorCode;
+        colorContainerEl.innerText = "#" + newColorCode;
+    });
+}
+
+function randomColor(){
+    const chars ="0123456789abcdf";
+    const colorCodeLength = 6;
+    let colorCode = "";
+    for (let index = 0; index < colorCodeLength; index++) {
+        const randomNumber = Math.floor(Math.random() * chars.length);
+        colorCode += chars.substring(randomNumber, randomNumber + 1);
+      
+    }
+    return colorCode;
+}  
